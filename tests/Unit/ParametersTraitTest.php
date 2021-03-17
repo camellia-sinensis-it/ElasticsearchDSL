dc0fb6915180690800e3ef452befe5bf644cdbbb
Pavle Predic <p.predic.extern@talentry.com>
2021-03-17 12:03:55 -0400

    PHP 8.0 support
    (cherry picked from commit 90be679a64953ae7d947b3a4c64c307a77f97b7e)

--- tests/Unit/ParametersTraitTest.php
+++ tests/Unit/ParametersTraitTest.php
@@ -27,7 +27,7 @@ class ParametersTraitTest extends \PHPUnit\Framework\TestCase
     /**
      * {@inheritdoc}
      */
-    public function setUp(): void
+    protected function setUp(): void
     {
         $this->parametersTraitMock = $this->getMockForTrait('ONGR\ElasticsearchDSL\ParametersTrait');
     }
